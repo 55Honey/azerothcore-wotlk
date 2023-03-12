faf56a2f8da086e83a18fb0f6875a559ef634ebb
55Honey <71938210+55Honey@users.noreply.github.com>
2023-03-12 15:30:07 +0100

    heal log attempt

--- src/server/game/Scripting/ScriptMgr.h
+++ src/server/game/Scripting/ScriptMgr.h
@@ -470,6 +470,8 @@ public:
     virtual void OnUnitEnterEvadeMode(Unit* /*unit*/, uint8 /*evadeReason*/) { }
     virtual void OnUnitEnterCombat(Unit* /*unit*/, Unit* /*victim*/) { }
     virtual void OnUnitDeath(Unit* /*unit*/, Unit* /*killer*/) { }
+
+    virtual void BeforeSendHeal(HealInfo* /*healInfo*/) { }
 };
 
 class MovementHandlerScript : public ScriptObject
@@ -2466,6 +2468,7 @@ public: /* UnitScript */
     void OnUnitEnterEvadeMode(Unit* unit, uint8 why);
     void OnUnitEnterCombat(Unit* unit, Unit* victim);
     void OnUnitDeath(Unit* unit, Unit* killer);
+    void BeforeSendHeal(HealInfo* healInfo);
 
 public: /* MovementHandlerScript */
     void OnPlayerMove(Player* player, MovementInfo movementInfo, uint32 opcode);

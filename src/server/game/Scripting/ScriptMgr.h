fc5002d73445c539e9fe3865ea566de862a51827
55Honey <71938210+55Honey@users.noreply.github.com>
2023-03-12 18:12:03 +0100

    Revert "heal log attempt"
    This reverts commit faf56a2f8da086e83a18fb0f6875a559ef634ebb.

--- src/server/game/Scripting/ScriptMgr.h
+++ src/server/game/Scripting/ScriptMgr.h
@@ -470,8 +470,6 @@ public:
     virtual void OnUnitEnterEvadeMode(Unit* /*unit*/, uint8 /*evadeReason*/) { }
     virtual void OnUnitEnterCombat(Unit* /*unit*/, Unit* /*victim*/) { }
     virtual void OnUnitDeath(Unit* /*unit*/, Unit* /*killer*/) { }
-
-    virtual void BeforeSendHeal(HealInfo* /*healInfo*/) { }
 };
 
 class MovementHandlerScript : public ScriptObject
@@ -2468,7 +2466,6 @@ public: /* UnitScript */
     void OnUnitEnterEvadeMode(Unit* unit, uint8 why);
     void OnUnitEnterCombat(Unit* unit, Unit* victim);
     void OnUnitDeath(Unit* unit, Unit* killer);
-    void BeforeSendHeal(HealInfo* healInfo);
 
 public: /* MovementHandlerScript */
     void OnPlayerMove(Player* player, MovementInfo movementInfo, uint32 opcode);
